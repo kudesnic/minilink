5dcd7190b0c9c1eda39e4f84c1dd0cba21e3f6bc
andrey <ANDRII.DEREVIANKO@DAXX.COM>
2020-11-10 11:50:20 +0200

    link tracking

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
 sh get-docker.sh
 #composer create-project symfony/skeleton app
 docker-compose build --no-cache
-docker-compose up
+docker-compose up -d
 docker-compose exec php composer install
 docker-compose exec php php bin/console doctrine:shema:create --force
 docker-compose exec php php bin/console geoip2:update
+mkdir -p config/jwt
+openssl genpkey -out config/jwt/private.pem -aes256 -algorithm rsa -pkeyopt rsa_keygen_bits:4096
+openssl pkey -in config/jwt/private.pem -out config/jwt/public.pem -pubout
