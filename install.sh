07cda7c5a4391a807bd3afb5535ec0512aa3a51e
andrey <ANDRII.DEREVIANKO@DAXX.COM>
2021-05-09 14:57:49 +0300

    removed some file and added command-generator of a trigger

--- install.sh
+++ install.sh
@@ -1,12 +1,11 @@
-#sh get-docker.sh
+sh get-docker.sh
 #composer create-project symfony/skeleton app
-#docker-compose build --no-cache
-#docker-compose up -d
-#docker-compose exec php php composer.phar install
-docker-compose exec php php bin/console doctrine:schema:create
-docker-compose exec php bin/console do:migrations:execute 20210404113226 --up
-docker-compose exec php php bin/console doctrine:fixtures:load
+docker-compose build --no-cache
+docker-compose up -d
+docker-compose exec php composer install
+docker-compose exec php php bin/console doctrine:shema:create --force
+docker-compose exec php bin/console app:create-guid-generator-pgs-trigger
 docker-compose exec php php bin/console geoip2:update
-#mkdir -p config/jwt
-#openssl genpkey -out backend/config/jwt/private.pem -aes256 -algorithm rsa -pkeyopt rsa_keygen_bits:4096
-#openssl pkey -in backend/config/jwt/private.pem -out backend/config/jwt/public.pem -pubout
+mkdir -p config/jwt
+openssl genpkey -out backend/config/jwt/private.pem -aes256 -algorithm rsa -pkeyopt rsa_keygen_bits:4096
+openssl pkey -in backend/config/jwt/private.pem -out backend/config/jwt/public.pem -pubout
